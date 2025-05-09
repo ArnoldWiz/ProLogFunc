2e11f6208585e48f87e98978db454bd8a532082b
ArnoldWiz <166636441+ArnoldWiz@users.noreply.github.com>
2025-05-08 21:23:57 -0600

    Create yerberito.pl

---
+++ yerberito/yerberito.pl
@@ -0,0 +1,79 @@
+enfermedad(problemas_digestivos).
+enfermedad(resfriado).
+enfermedad(fiebre).
+enfermedad(dolor_de_cabeza).
+enfermedad(insomnio).
+enfermedad(ansiedad).
+enfermedad(colicos).
+enfermedad(indigestion).
+enfermedad(infeccion_urinaria).
+enfermedad(enfermedad_renal).
+
+sintoma_enfermedad(problemas_digestivos, dolor_abdominal).
+sintoma_enfermedad(problemas_digestivos, gases).
+sintoma_enfermedad(resfriado, tos).
+sintoma_enfermedad(resfriado, congestion_nasal).
+sintoma_enfermedad(fiebre, temperatura_alta).
+sintoma_enfermedad(fiebre, escalofrios).
+sintoma_enfermedad(dolor_de_cabeza, presion_en_la_cabeza).
+sintoma_enfermedad(insomnio, dificultad_para_dormir).
+sintoma_enfermedad(ansiedad, nerviosismo).
+sintoma_enfermedad(colicos, dolor_intestinal).
+sintoma_enfermedad(indigestion, pesadez_estomacal).
+sintoma_enfermedad(infeccion_urinaria, ardor_al_orinar).
+sintoma_enfermedad(enfermedad_renal, dolor_en_los_riñones).
+
+nombre_cientifico(oregano, origanum_vulgare).
+contienente_origen(oregano, europa).
+pais_origen(oregano, region_mediterranea).
+modo_preparacion(oregano, infusion).
+trata_enfermedad(oregano, problemas_digestivos).
+trata_enfermedad(oregano, resfriado).
+accion_efecto_planta(oregano, antiespasmodico).
+accion_efecto_planta(oregano, expectorante).
+modo_tratamiento(oregano, dos_veces_dia).
+precaucion_planta(oregano, evitar_dosis_altas).
+
+nombre_cientifico(palo_de_flor, bourreria_huanita).
+contienente_origen(palo_de_flor, america).
+pais_origen(palo_de_flor, mexico).
+modo_preparacion(palo_de_flor, cocimiento).
+trata_enfermedad(palo_de_flor, fiebre).
+trata_enfermedad(palo_de_flor, dolor_de_cabeza).
+accion_efecto_planta(palo_de_flor, febrifugo).
+accion_efecto_planta(palo_de_flor, analgésico).
+modo_tratamiento(palo_de_flor, dos_veces_dia).
+precaucion_planta(palo_de_flor, no_prolongar_tratamiento).
+
+nombre_cientifico(pasiflora, passiflora_incarnata).
+contienente_origen(pasiflora, america).
+pais_origen(pasiflora, mexico).
+modo_preparacion(pasiflora, infusion).
+trata_enfermedad(pasiflora, insomnio).
+trata_enfermedad(pasiflora, ansiedad).
+accion_efecto_planta(pasiflora, sedante).
+accion_efecto_planta(pasiflora, ansiolítico).
+modo_tratamiento(pasiflora, antes_de_dormir).
+precaucion_planta(pasiflora, no_combinar_con_alcohol).
+
+nombre_cientifico(pericon, tagetes_lucida).
+contienente_origen(pericon, america).
+pais_origen(pericon, mexico).
+modo_preparacion(pericon, infusion).
+trata_enfermedad(pericon, colicos).
+trata_enfermedad(pericon, indigestión).
+accion_efecto_planta(pericon, digestivo).
+accion_efecto_planta(pericon, carminativo).
+modo_tratamiento(pericon, despues_comidas).
+precaucion_planta(pericon, evitar_embarazo).
+
+nombre_cientifico(pinguica, arctostaphylos_pungens).
+contienente_origen(pinguica, america).
+pais_origen(pinguica, mexico).
+modo_preparacion(pinguica, cocimiento).
+trata_enfermedad(pinguica, infeccion_urinaria).
+trata_enfermedad(pinguica, riñones).
+accion_efecto_planta(pinguica, diuretico).
+accion_efecto_planta(pinguica, depurativo).
+modo_tratamiento(pinguica, tres_veces_dia).
+precaucion_planta(pinguica, no_usar_por_periodos_largos).
